74486b925892398867121d9da9b674a083c11319
Artimoni <76207858+Artimoni@users.noreply.github.com>
2024-12-06 14:57:13 +0800

    Add files via upload

---
+++ zad21.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int m;
+    cout << "Введите размер массива (2n + 1): ";
+    cin >> m;
+
+    vector<int> A(m);
+
+    cout << "Введите элементы массива (не одинаковые): ";
+    for (int i = 0; i < m; i++) {
+        cin >> A[i];
+    }
+
+    sort(A.begin(), A.end());
+
+    int median = A[m / 2];
+
+    cout << "Медиана: " << median << endl;
+
+    return 0;
+}
